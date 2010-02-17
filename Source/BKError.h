52ef8b6c79709c2023d5fcc0d958324a237dffcf
Lukhnos D. Liu <lukhnos@lithoglyph.com>
2010-02-17 16:32:26 +0800

    Supplies FogBugz error code.

--- Source/BKError.h
+++ Source/BKError.h
@@ -27,14 +27,42 @@
 
 #import <Foundation/Foundation.h>
 
+// TODO: Remove these
 extern NSString *const BKConnectionErrorDomain;
 extern NSString *const BKAPIErrorDomain;
 
 typedef enum {
+    // TODO: Remove these
 	BKConnecitonLostError = -1,
 	BKConnectionTimeoutError = -2,
 	BKConnectionServerHTTPError = -3,	// e.g. 404
 	
 	BKAPIMalformedResponseError = -100,
-	BKUnknownError = -9999
+	BKUnknownError = -9999,
+	
+	BKNotInitializedError = 0,
+	BKLogOnIncorrectUsernameOrPasswordError = 1,
+	BKLogOnMultipleMatchesForUsernameError = 2,
+	BKNotLoggedOnError = 3,
+	BKArgumentMissingFromQueryError = 4,
+	BKEditingCaseNotFoundError = 5,
+	BKEditingCaseActionNotPermitted = 6,
+	BKTimeTrackingError = 7,
+	BKNewCaseCannotWriteToAnyProjectError = 8,
+	BKCaseChangedSinceLastViewError = 9,
+	BKSearchError = 10,
+	BKWikiCreationError = 12,
+	BKWikiPermissionError = 13,
+	BKWikiLoadError = 14,
+	BKWikiTemplateError = 15,
+	BKWikiCommitError = 16,
+	BKNoSuchProjectError = 17,
+	BKNoSuchUserError = 18,
+	BKAreaCreationError = 19,
+	BKMilestoneCreationError = 20,
+	BKProjectCreationError = 21,
+	BKUserCreationError = 22,
+	BKProjectPercentTimeError = 23,
+	BKNoSuchMilestoneError = 24,
+	BKMilestoneExecutionOrderViolationError = 25
 } BKErrorCode;
